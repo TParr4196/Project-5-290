623682960e65bc6ae45186b5405e72a31aea1e1e
Tyler Parr <parrri@oregonstate.edu>
2022-02-06 22:40:34 -0800

    more cleanup

--- llist.c
+++ llist.c
@@ -5,18 +5,20 @@
 
 int main(int argc, const char *argv[]) {
   int i;
-  node head;
+  struct node *head=NULL;
   for(i=0; a<argc; i++){
     if(argv[i].equals("ih"){
-      llist_insert_head(head, atoi(argv[i+1]));
+      struct node *n = node_alloc(atoi(argv[i+1]));
+      llist_insert_head(&head, n);
       i++;
     }
     if(argv[i].equals("it"){
-      llist_insert_tail(head, atoi(argv[i+1]));
+      struct node *n = node_alloc(atoi(argv[i+1]));
+      llist_insert_tail(&head, n);
       i++;
     }
     if(argv[i].equals("dh"){
-      llist_delete_head(head);
+      llist_delete_head(&head);
     }
     if(argv[i].equals("f"){
       llist_free(head);
@@ -25,4 +27,32 @@ int main(int argc, const char *argv[]) {
       llist_print(head);
     }
   }
+}
+
+void llist_insert_head(struct node **head, struct node *n){
+
+}
+
+struct node *llist_delete_head(struct node **head){
+
+}
+
+void llist_insert_tail(struct node **head, struct node *n){
+
+}
+
+void llist_print(struct node *head){
+
+}
+
+void llist_free(struct node **head){
+
+}
+
+struct node *node_alloc(int value){
+  
+}
+
+void node_free(struct node *n){
+
 }
